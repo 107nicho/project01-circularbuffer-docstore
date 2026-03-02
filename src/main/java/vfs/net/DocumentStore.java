0eaea02ba5e52eea519eb714cdf8060b2b4ab6c0
Nicho Bauder <nbauder107@gmail.com>
2026-03-02 01:40:26 -0600

    initial commit: buffer skeleton

---
+++ src/main/java/vfs/net/DocumentStore.java
@@ -0,0 +1,19 @@
+package vfs.net;
+
+public class DocumentStore {
+    public void addDocument(Document d) {
+
+    }
+
+    undoLast() {
+
+    }
+
+    public Document getLastK(int k) {
+
+    }
+
+    replayAll() {
+
+    }
+}
